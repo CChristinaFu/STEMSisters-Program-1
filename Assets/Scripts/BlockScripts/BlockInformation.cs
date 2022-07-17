54d93ff46b44edfc49c81fe2c7779a4df48bf610
SurajSSingh <surajss2@illinois.edu>
2022-07-17 11:31:33 -0700

    07/17/2022 Pt 1
    Added Editor Toolbox Kit

--- Assets/Scripts/BlockScripts/BlockInformation.cs
+++ Assets/Scripts/BlockScripts/BlockInformation.cs
@@ -15,6 +15,22 @@ public class BlockInformation : ScriptableObject
 
 }
 public enum BlockType{ACTION, OPERATOR, CONTROL, VARIABLE, FUNCTION, EVENTS}
+// Action: Effect game state directly
+//  * Example: Water field
+//  * Example: Selling 
+// (Boolean) Operator: Operate on some variable
+//  * Pre-exisitng
+//  * Exists
+// Control: If and Loops
+//  * 
+// Function:
+//  * Function Call
+//  * Parameter Assignment
+// Events: 
+//  * START
+//  * Every n sec
+//  * def fn
+
 public enum VariableInputType{DROPDOWN, TYPEABLE, PREDICATE}
 public enum EventType{FROM_START, EVERY_N_SECONDS, DEFINE_FUNCTION}
 
