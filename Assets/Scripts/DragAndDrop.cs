0cfa5f68392d69a5ff7da280b6e8433dfa581ae2
CChristinaFu <108163507+CChristinaFu@users.noreply.github.com>
2022-08-12 20:31:03 -0700

    8/12/2022
    removed old images, updated top block, started separating drag and drop script.

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
-public class DragAndDrop : MonoBehaviour, 
-IDragHandler, IPointerDownHandler, IDropHandler, 
+public class DragAndDrop : MonoBehaviour,
+IDragHandler, IPointerDownHandler, IDropHandler,
 IEndDragHandler, IPointerClickHandler, IBeginDragHandler
 {
     private Vector3 startPosition;
     private Vector3 difPosition;
     [SerializeField] GameObject canvas;
     [SerializeField] GameObject panelGhost;
-
+    public UnityEvent OnStartDragging = new();
+    public UnityEvent OnDropBlock = new();
     public void OnBeginDrag(PointerEventData eventData)
     {
         gameObject.GetComponent<Image>().raycastTarget = false;
@@ -27,8 +29,8 @@ IEndDragHandler, IPointerClickHandler, IBeginDragHandler
     {
         EventSystem.current.currentSelectedGameObject.transform.parent = transform;
         float sizeY = GetComponent<RectTransform>().rect.size.y;
-        int index = (int)((((transform.position.y+(sizeY/2)) - Input.mousePosition.y))/35)-1;
-        if(index < 0)
+        int index = (int)((((transform.position.y + (sizeY / 2)) - Input.mousePosition.y)) / 35) - 1;
+        if (index < 0)
         {
             index = 0;
         }
@@ -40,7 +42,7 @@ IEndDragHandler, IPointerClickHandler, IBeginDragHandler
         // rebuilds the layout and its child elements (previously done in UIDrag)
         // the objects that allow drop are the ones who actually need this rebuild
         LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-
+        OnDropBlock.Invoke();
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -61,17 +63,18 @@ IEndDragHandler, IPointerClickHandler, IBeginDragHandler
         EventSystem.current.currentSelectedGameObject.transform.SetParent(canvas.transform);
         EventSystem.current.currentSelectedGameObject.transform.SetAsLastSibling();
         Debug.Log($"Started dragging: {gameObject.name}");
+        OnStartDragging.Invoke();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/DropUIScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class DropUIScript : MonoBehaviour
+{
+    public UnityEvent OnDropBlock = new();
+    public void OnDrop(PointerEventData eventData)
+    {
+        EventSystem.current.currentSelectedGameObject.transform.parent = transform;
+        float sizeY = GetComponent<RectTransform>().rect.size.y;
+        int index = (int)((((transform.position.y + (sizeY / 2)) - Input.mousePosition.y)) / 35) - 1;
+        if (index < 0)
+        {
+            index = 0;
+        }
+        Debug.Log(index);
+        EventSystem.current.currentSelectedGameObject.transform.SetSiblingIndex(index);
+        EventSystem.current.SetSelectedGameObject(null);
+        Debug.Log("drop " + gameObject.name);
+        Debug.Log(transform);
+        // rebuilds the layout and its child elements (previously done in UIDrag)
+        // the objects that allow drop are the ones who actually need this rebuild
+        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+        OnDropBlock.Invoke();
+    }
+}
